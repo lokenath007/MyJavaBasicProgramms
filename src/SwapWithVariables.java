eed94289bad97b440eb32fa8c6de615dc7609db5
lokenath007 <lokenathghosh@gmail.com>
2015-10-06 17:14:43 +0530

    SwappingusingThirdVariables

---
+++ src/SwapWithVariables.java
@@ -0,0 +1,19 @@
+
+public class SwapWithVariables {
+	
+	public static void main(String args[])
+	{
+		
+		
+		int a=10;
+		int b=20;
+		int temp;
+		System.out.println("Before swapping the values of a and b are "+ a+" "+b);
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("After swapping the values of a and b are "+ a+" "+b);
+	}
+
+}
